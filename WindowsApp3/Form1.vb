3d13682c3cf075b6f702866f1fc1f981c475b46c
lrh <9066200162QQ.COM>
2019-06-03 01:14:50 +0800

    更新了字符首字母查字体功能

--- WindowsApp3/Form1.vb
+++ WindowsApp3/Form1.vb
@@ -143,7 +143,7 @@ Public Class Form1
     End Sub
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
-        MsgBox(toPinyin("你好")）
+
     End Sub
 
     Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
@@ -153,17 +153,67 @@ Public Class Form1
     Private Sub ComboBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox4.SelectedIndexChanged
 
     End Sub
-    Public Function toPinyin(ByVal chars As String) As String
-        Dim pinyin As String = ""
-        Dim charArray() As Char = chars.ToCharArray
-        Dim ft As New Pinyin4net.Format.HanyuPinyinOutputFormat         '自定义转换格式
-        ft.VCharType = Pinyin4net.Format.HanyuPinyinVCharType.WITH_V    '使用v来表示韵母u,女=nv
-        ft.CaseType = Pinyin4net.Format.HanyuPinyinCaseType.LOWERCASE   '使用小写
-        ft.ToneType = Pinyin4net.Format.HanyuPinyinToneType.WITHOUT_TONE    '不带声标,否则会带数字第几声,例如:牛=niu2
-        For Each c As Char In charArray
-            pinyin &= Pinyin4net.PinyinHelper.ToHanyuPinyinStringArray(c, ft)(0)
-        Next
-        Return pinyin
+    Public Function HzTopy(ByVal mystr As String) As String
+        Dim i As Integer
+        Dim J As Integer
+        Dim Pstr As String
+        Try
+            Dim k As Integer = Len(mystr)
+            For J = 1 To k
+                i = Asc(Mid(mystr, J, 1))
+
+                Dim Py As String
+                Select Case i
+                    Case -20319 To -20284 : Py = "A"
+                    Case -20283 To -19776 : Py = "B"
+                    Case -19775 To -19219 : Py = "C"
+                    Case -19218 To -18711 : Py = "D"
+                    Case -18710 To -18527 : Py = "E"
+                    Case -18526 To -18240 : Py = "F"
+                    Case -18239 To -17923 : Py = "G"
+                    Case -17922 To -17418 : Py = "H"
+                    Case -17417 To -16475 : Py = "J"
+                    Case -16474 To -16213 : Py = "K"
+                    Case -16212 To -15641 : Py = "L"
+                    Case -15640 To -15166 : Py = "M"
+                    Case -15165 To -14923 : Py = "N"
+                    Case -14922 To -14915 : Py = "O"
+                    Case -14914 To -14631 : Py = "P"
+                    Case -14630 To -14150 : Py = "Q"
+                    Case -14149 To -14091 : Py = "R"
+                    Case -14090 To -13319 : Py = "S"
+                    Case -13318 To -12839 : Py = "T"
+                    Case -12838 To -12557 : Py = "W"
+                    Case -12556 To -11848 : Py = "X"
+                    Case -11847 To -11056 : Py = "Y"
+                    Case -11055 To -10247 : Py = "Z"
+                    Case Else : Py = CStr(Chr(i))
+                End Select
+                Pstr = Pstr & Py
+            Next
+            HzTopy = Pstr
+        Catch ex As Exception
+            MsgBox("转成失败！")
+            Return String.Empty
+        End Try
     End Function
 
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+        For Each fonty In FontFamily.Families
+            If fonty.Name <> "" Then
+
+                If Asc(fonty.Name) < 0 Then
+                    If TextBox4.Text.ToUpper = HzTopy(fonty.Name) Then
+
+                        ComboBox3.Text = fonty.Name
+
+                    End If
+
+
+                End If
+
+            End If
+        Next
+    End Sub
 End Class
